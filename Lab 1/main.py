7ed66ba915fcd91ffd271aba0d55a28c56d2a702
Maciekxdabu <40292375+Maciekxdabu@users.noreply.github.com>
2023-03-17 17:32:04 +0100

    Trying to find start

--- Lab 1/main.py
+++ Lab 1/main.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 # O - empty, X - wall, S - start, E - end
 maze = [['X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X'],
         ['X', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'X'],
@@ -11,3 +13,22 @@ maze = [['X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X'],
         ['X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X'],
         ['X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X']]
 
+result = list(zip(*np.where(maze == 'S')))
+print(result)
+
+#print(maze.index('S'))
+# maze.where
+# for rows in maze:
+#     print(maze.index('S'))
+
+
+# frontier = 
+# explored = []
+
+# class State:
+#     x = -1
+#     y = -1
+#     h = 100000000
+
+# def h(s):
+#     return 5
