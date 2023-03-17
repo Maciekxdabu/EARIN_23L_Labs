3b1cdd021a05e364f014ed8bac2589180fad8b42
Gabriel Ksawery Skowron-Rodriguez <gsrodriguez@wp.pl>
2023-03-17 17:45:34 +0100

    found way to get index of start position

--- Lab 1/main.py
+++ Lab 1/main.py
@@ -1,28 +1,28 @@
 import numpy as np
 
 # O - empty, X - wall, S - start, E - end
-maze = [['X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X'],
-        ['X', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'X'],
-        ['X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'O', 'X'],
-        ['X', 'O', 'O', 'O', 'O', 'S', 'O', 'O', 'O', 'X'],
-        ['X', 'O', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X'],
-        ['X', 'O', 'X', 'O', 'O', 'O', 'O', 'O', 'O', 'X'],
-        ['X', 'O', 'X', 'O', 'X', 'X', 'X', 'X', 'O', 'X'],
-        ['X', 'O', 'O', 'O', 'X', 'X', 'X', 'X', 'O', 'X'],
-        ['X', 'X', 'X', 'X', 'E', 'O', 'O', 'O', 'O', 'X'],
-        ['X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X'],
-        ['X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X']]
+maze = np.array([['X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X'],
+                 ['X', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'X'],
+                 ['X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'O', 'X'],
+                 ['X', 'O', 'O', 'O', 'O', 'S', 'O', 'O', 'O', 'X'],
+                 ['X', 'O', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X'],
+                 ['X', 'O', 'X', 'O', 'O', 'O', 'O', 'O', 'O', 'X'],
+                 ['X', 'O', 'X', 'O', 'X', 'X', 'X', 'X', 'O', 'X'],
+                 ['X', 'O', 'O', 'O', 'X', 'X', 'X', 'X', 'O', 'X'],
+                 ['X', 'X', 'X', 'X', 'E', 'O', 'O', 'O', 'O', 'X'],
+                 ['X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X'],
+                 ['X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X']])
 
-result = list(zip(*np.where(maze == 'S')))
+result = np.argwhere(maze == 'S')
 print(result)
 
-#print(maze.index('S'))
+# print(maze.index('S'))
 # maze.where
 # for rows in maze:
 #     print(maze.index('S'))
 
 
-# frontier = 
+# frontier =
 # explored = []
 
 # class State:
