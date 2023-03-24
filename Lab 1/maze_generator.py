ddf0b93d4c561cf162ef79e85de14d880e403480
Gabriel Ksawery Skowron-Rodriguez <gsrodriguez@wp.pl>
2023-03-24 18:15:01 +0100

    template for maze generator

---
+++ Lab 1/maze_generator.py
@@ -0,0 +1,16 @@
+import constants as c
+import gui
+
+import numpy as np
+import random
+
+
+def generate_maze(height, width):
+
+    return maze
+
+
+maze = generate_maze(11, 11)
+
+gui.initConsole()
+gui.printMazeWithFrame(maze=maze)
