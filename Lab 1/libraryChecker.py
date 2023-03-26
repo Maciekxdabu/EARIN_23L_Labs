98d138b1c01be3515da1e63ed4a4286d6e2848f4
Gabriel Ksawery Skowron-Rodriguez <gsrodriguez@wp.pl>
2023-03-26 15:06:24 +0200

    Create libraryChecker.py

---
+++ Lab 1/libraryChecker.py
@@ -0,0 +1,18 @@
+import pkg_resources
+
+# get a list of all installed packages and their version numbers
+installed_packages = pkg_resources.working_set
+
+# loop over the installed packages and check if they require pip install
+for package in installed_packages:
+    if package.key.startswith('pip'):
+        continue  # skip pip packages
+    try:
+        package_dist = pkg_resources.get_distribution(package.key)
+        if package_dist.has_metadata('RECORD'):
+            print(
+                f"{package.key} is not included in Python standard library or was not installed via pip.")
+            continue  # package was installed via pip
+    except:
+        pass
+   # print(f"{package.key} is included in Python standard library or was not installed via pip.")
